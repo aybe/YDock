bb1a82231b8b4c988cdb6d2833f263a3e4b17373
aybe <aybe@users.noreply.github.com>
2022-03-18 07:49:18 +0100

    Improve floating window header
    - use system parameters for buttons dimensions
    - remove hardcoded values

---
+++ YDock/Converters/AddConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace YDock.Converters;
+
+internal sealed class AddDoublesConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        return values.Cast<double>().Sum();
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
